2dbed1a44547c16b4b640b8d31d5d552e33794c6
Kelvin Mue <kelvinmue@gmail.com>
2023-09-13 14:57:39 +0300

    Completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(arr):
+    if len(arr) == 1:
+        return arr[0]
+    elif len(arr) == 2:
+        return f"{arr[0]} and {arr[1]}"
+    else:
+        oxford_string = ', '.join(arr[:-1])
+        oxford_string += f", and {arr[-1]}"
+        return oxford_string
